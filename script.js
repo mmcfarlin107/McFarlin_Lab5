3ec39ce42199c909375ab2aa37f1c404e70d2a2b
mmcfarlin107 <mcfarlin107@gmail.com>
2017-06-29 20:02:34 -0400

    My First JS Project

---
+++ script.js
@@ -0,0 +1,9 @@
+var temp = parseInt(prompt("What is the temperature in Farenheit?"));
+// console.log((temp - 32) * (5/9) + "Degrees Celcius");
+console.log(temp + " degrees Fahrenheit equals " + ((temp - 32) * (5 / 9)) + " degrees Celsius");
+
+
+var temp2 = parseInt(prompt("What is the temperature in Celsius?"));
+console.log(temp2 + " degrees Celsius equals " + (temp2 * (9 / 5) + 32) + " degrees Fahrenheit");
+
+//T(°F) = T(°C) × 9/5 + 32
